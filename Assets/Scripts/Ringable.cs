a7fbacdead950d33be1d9f2069f7c749c8ebdd51
Dylan Abramson <dabramson@wesleyan.edu>
2020-05-16 17:31:54 -0400

    Ringable

---
+++ Assets/Scripts/Ringable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ringable : MonoBehaviour
+{
+
+    public abstract void Ring();
+
+    
+    
+}
